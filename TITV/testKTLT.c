05fdd4980b7cbb2434aec6fe4a74780c7c9ddda2
Dang Dinh Nguyen <banhvannguyen45@gmail.com>
2026-03-26 13:24:14 +0700

    Bai 23

--- TITV/testKTLT.c
+++ TITV/testKTLT.c
@@ -4,10 +4,17 @@
 //g++ "testKLT.c" -o main; ./main 
 
 int main(){
-  char arrCourseName[][11] = {"JavaScript", "NodeJS", "Python"};
-  int numCourses = sizeof(arrCourseName) / sizeof(arrCourseName[0]);
+  int arrInt[] = {2, 3, 6};
+  char arrString[][2] = { "S", "Z", "T", "W" };
+  int sumEven = 0;
+  int arrIntLength = sizeof(arrInt)/sizeof(int);
 
-  for (int i = 0; i < numCourses; i++) {
-    printf("%s ", arrCourseName[i]);
+  for(int i=0; i < arrIntLength; i++) {
+    if(arrInt[i] % 2 == 0) {
+        sumEven += arrInt[i];
+    }
+    arrString[i][1] = sumEven + '0';
   }
+
+  printf("%s", arrString[1]);
 }
