dbb41d72f7172628363897e78bd670348139e749
Dang Dinh Nguyen <banhvannguyen45@gmail.com>
2026-09-18 15:22:34 +0700

    feat(demcs): write a program to counting parameter of a number

---
+++ DSA-Data Structures and Algorithms/Olympic/Exercise/demcs.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+long long demCS(long long n);
+
+long long demCS(long long n) {
+    if(n == 0 ) return 1;
+    else if(n<0) return -n;
+    int count=0;
+    while(n >0) {
+        count++;
+        n = n/10;
+    }
+    return count;
+}
+
+
+
+int main() {
+    long long n;
+    scanf("%lld", &n);
+    printf("%lld", demCS(n));
+    return 0;
+}
