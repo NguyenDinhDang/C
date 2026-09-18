afa6e4125e26b2c4f9b461a4cdbb312c843be8da
Dang Dinh Nguyen <banhvannguyen45@gmail.com>
2026-09-18 15:14:36 +0700

    feat(ucln): add a funtion to slove lcm

--- DSA-Data Structures and Algorithms/Olympic/Exercise/ucln.c
+++ DSA-Data Structures and Algorithms/Olympic/Exercise/ucln.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 long long gcd(long long a, long long b) {
     long long temp;
@@ -9,12 +10,18 @@ long long gcd(long long a, long long b) {
     return gcd(b, temp);
 }
 
-void testGcd() {
+long long lcm(long long a, long long b) {
+    if (a == 0 || b == 0) return 0;
+    return llabs((a / gcd(a, b)) * b);
+}
+
+void test() {
     long long a = 252;
     long long b = 105;
-    printf("%lld",gcd(a, b));
+    printf("gcd: %lld\n",gcd(a, b));
+    printf("lcm: %lld\n", lcm(a, b));
 }
 
 int main() {
-    testGcd();
+    test();
 }
